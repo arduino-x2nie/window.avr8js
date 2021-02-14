d7ba9595c8c00568bf14216c2d9b3675cb00c883
x2nie <x2nie@yahoo.com>
2021-02-14 12:31:36 +0700

    first commit

---
+++ vendor/src/index.js
@@ -0,0 +1,1 @@
+window.avr8js = require("avr8js");
---
+++ vendor/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'avr8js.js',
+    path: path.resolve(__dirname, '..', 'static'),
+  },
+};
